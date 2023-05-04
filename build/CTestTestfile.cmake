801c13b8e9f6e16ae9d3f4291b40345fa57cafec
Shivansh2703 <shivansh.sonit@gmail.com>
2023-05-04 14:38:08 +0530

    Setting up

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shivansh/catkin_ws/src
-# Build directory: /home/shivansh/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
